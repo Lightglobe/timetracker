25b8ae33f2cf22d8a70a54c593b722b3cddc2552
Lightglobe <alextsambalos@gmail.com>
2020-11-12 11:33:34 +0200

    CRUD implementation in TimerDashboard: Loading timers, start timer, stop timer, update timer attributes,delete timer

--- src/app/layout/features/dashboard/Header.js
+++ src/app/layout/features/dashboard/Header.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div class="ui center aligned text header">
-                <br/>
-                <h1>Time tracking</h1>
-                <hr/>
-            </div>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div className="ui center aligned text header">
+        <br />
+        <h1>Time tracking</h1>
+        <hr />
+      </div>
+    );
+  }
 }
+export default Header;
--- src/app/layout/features/dashboard/TimerDashboard.js
+++ src/app/layout/features/dashboard/TimerDashboard.js
@@ -1,119 +1,194 @@
-import React, { Component } from 'react'
-import EditableTimerList from '../timers/EditableTimerList'
-import Header from './Header';
-import {v4 as uuidv4} from 'uuid';
-import ToggleableTimerForm from '../timers/ToggleableTimerForm';
+import React, { Component } from "react";
+import EditableTimerList from "../timers/EditableTimerList";
+import Header from "./Header";
+import { v4 as uuidv4 } from "uuid";
+import ToggleableTimerForm from "../timers/ToggleableTimerForm";
 
 export default class TimerDashboard extends Component {
-    state = {
-        timers: [
-            {
-                title: 'Practise deadlift',
-                project: 'Gym chores',
-                id: uuidv4(),
-                elapsed: 5456099,
-                runningSince: Date.now()
-            },
-            {
-                title: 'Practise squat',
-                project: 'Gym chores',
-                id: uuidv4(),
-                elapsed: 1273998,
-                runningSince: null
-            }
-        ]
-    };
+  state = {
+    timers: [],
+  };
 
-    handleTrashClick = (timerId) => {
-        this.setState({
-            timers: this.state.timers.filter(t => t.id !== timerId),
-        })
-    }
-    handleEditFormSubmit = (attrs) => {
-        this.setState({
-            timers: this.state.timers.map((timer) => {
-                if(timer.id === attrs.id) {
-                    return Object.assign({}, timer, {
-                        title: attrs.title,
-                        project: attrs.project
-                    });
-                }
-                else
-                {
-                    return timer;
-                }
-            }),
-        });
-    }
+  componentDidMount() {
+    this.loadTimersFromServer();
+    //setInterval(this.loadTimersFromServer, 5000);
+  }
 
-    handleCreateFormSubmit = (timer) => {
-        const t = this.createTimer(timer);
-        this.setState({timers: this.state.timers.concat(t)});
-    }
+  loadTimersFromServer = () => {
+    this.getTimers((serverTimers) =>
+      this.setState({
+        timers: serverTimers,
+      })
+    );
+  };
 
-    createTimer = (timer) => {
-        const t = {
-            title: timer.title || 'Timer',
-            project: timer.project || 'Project',
-            id: uuidv4(),
-            elapsed: 0,
+  getTimers = (success) => {
+    return fetch("/api/timers", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then(this.checkStatus)
+      .then((res) => res.json())
+      .then(success);
+  };
+
+  checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      const error = new Error(`HTTP Error ${response.statusText}`);
+      error.status = response.statusText;
+      error.response = response;
+      console.log(error);
+      throw error;
+    }
+  };
+
+  handleTrashClick = (timerId) => {
+    this.setState({
+      timers: this.state.timers.filter((t) => t.id !== timerId),
+    });
+    this.deleteTimer({ id: timerId });
+  };
+
+  deleteTimer(data) {
+    return fetch("/api/timers", {
+      method: "delete",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(this.checkStatus);
+  }
+
+  handleEditFormSubmit = (attrs) => {
+    this.setState({
+      timers: this.state.timers.map((timer) => {
+        if (timer.id === attrs.id) {
+          return Object.assign({}, timer, {
+            title: attrs.title,
+            project: attrs.project,
+          });
+        } else {
+          return timer;
         }
-        return t;
-    }
-    
-    handleStartClick = (timerId) => {
-        const now = Date.now();
-        this.setState({
-            timers: this.state.timers.map((timer) => {
-                if(timer.id === timerId){
-                    return Object.assign({}, timer, {
-                        runningSince: now,
-                    });
-                }else{
-                    return timer;
-                }
-            }),
-        });
-    }
+      }),
+    });
+    this.updateTimer(attrs);
+  };
 
-    handleStopClick = (timerId) => {
-        const now = Date.now();
-        this.setState({
-            timers: this.state.timers.map((timer) => {
-                if(timer.id === timerId){
-                    const lastElapsed = now - timer.runningSince;
-                    return Object.assign({},timer,{
-                        elapsed: timer.elapsed + lastElapsed,
-                        runningSince: null,
-                    });
-                }else{
-                    return timer;
-                }
-            })
-        })
-    }
+  updateTimer(data) {
+    return fetch("/api/timers", {
+      method: "put",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(this.checkStatus);
+  }
 
+  handleCreateFormSubmit = (timer) => {
+    const t = this.createTimer(timer);
+    this.setState({ timers: this.state.timers.concat(t) });
+    this.createServerTimer(t);
+  };
 
+  createTimer = (timer) => {
+    const t = {
+      title: timer.title || "Timer",
+      project: timer.project || "Project",
+      id: uuidv4(),
+      elapsed: 0,
+    };
+    return t;
+  };
 
-    render() {
+  createServerTimer(data) {
+    return fetch("/api/timers", {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(this.checkStatus);
+  }
+  handleStartClick = (timerId) => {
+    const now = Date.now();
+    this.setState({
+      timers: this.state.timers.map((timer) => {
+        if (timer.id === timerId) {
+          return Object.assign({}, timer, {
+            runningSince: now,
+          });
+        } else {
+          return timer;
+        }
+      }),
+    });
+    this.startTimer({ id: timerId, start: now });
+  };
 
-        return (
-            <React.Fragment>
-                <Header/>
-                <div className='ui three column centered grid'>
-                    <div className='column'>
-                        <EditableTimerList 
-                            onTrashClick={this.handleTrashClick}
-                            onFormSubmit={this.handleEditFormSubmit}
-                            timers={this.state.timers}
-                            onStartClick={this.handleStartClick}
-                            onStopClick={this.handleStopClick}
-                        />
-                        <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
-                    </div>
-                </div>
-            </React.Fragment>
+  startTimer(data) {
+    return fetch("api/timers/start", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(this.checkStatus);
+  }
 
-        )
-    }
+  handleStopClick = (timerId) => {
+    const now = Date.now();
+    this.setState({
+      timers: this.state.timers.map((timer) => {
+        if (timer.id === timerId) {
+          const lastElapsed = now - timer.runningSince;
+          return Object.assign({}, timer, {
+            elapsed: timer.elapsed + lastElapsed,
+            runningSince: null,
+          });
+        } else {
+          return timer;
+        }
+      }),
+    });
+    this.stopTimer({ id: timerId, stop: now });
+  };
+
+  stopTimer(data) {
+    return fetch("/api/timers/stop", {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then(this.checkStatus);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div className="ui three column centered grid">
+          <div className="column">
+            <EditableTimerList
+              onTrashClick={this.handleTrashClick}
+              onFormSubmit={this.handleEditFormSubmit}
+              timers={this.state.timers}
+              onStartClick={this.handleStartClick}
+              onStopClick={this.handleStopClick}
+            />
+            <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
